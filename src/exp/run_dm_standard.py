96dfd7c4e96a73ea95745f3b19f2a7297651dca9
zz <ziqizhang.email@gmail.com>
2021-02-16 14:27:40 +0000

    zz

--- src/exp/run_dm_standard.py
+++ src/exp/run_dm_standard.py
@@ -97,8 +97,8 @@ if __name__ == "__main__":
     optim.set_parameters(model.named_parameters())
     start = time.time()
 
-    if len(sys.arg>5):
-        ep=int(sys.arg[5])
+    if len(sys.args>5):
+        ep=int(sys.args[5])
     else:
         ep=30
     model.run_train(
